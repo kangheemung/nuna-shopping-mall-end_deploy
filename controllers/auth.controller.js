06ede180149dc561b7dd73d7b550e4f6c5451bce
kurotamashi30 <kurotamashi30@gmail.com>
2024-10-26 22:49:38 +0900

    bcryptjs

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;

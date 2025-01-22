6fc18a7d69555cb3dc4dc40dd4195779fdf2036e
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-01-22 22:38:35 +0900

    product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -24,6 +24,22 @@ productController.createProduct = async (req, res) => {
 };
 productController.getProducts = async (req, res) => {
     try {
+        const {page,name}= req.query
+        const cond =name?{name:{$regex:name,$option:"i"}}:{}
+        let query =Product.find(cond)
+        const productList=await query.exec();
+        // if (name){
+        //     const products =await Product.find({name:{$regex:name,$option:"i"}})
+        // }else {
+        //     const products=await Product.find({});
+        // }
+        // else if(page){
+        //     if (name){
+        //         const products =await Product.find({name:{$regex:name,$option:"i"}}).limit();
+        //     }else{
+        //         products=awaitProduct.find().limit;
+        //     }
+        //}
         const products = await Product.find({});
         res.status(200).json({ status: 'succuss', data: products });
     } catch (err) {

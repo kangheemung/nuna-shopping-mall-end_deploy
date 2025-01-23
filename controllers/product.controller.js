9e2a5ddce9db1ac1c1ecb603a6f0ae3163544cae
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-01-23 22:18:04 +0900

    test

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Product = require('../models/Product');
 
 const productController = {};
-
+const PAGE_SIZE = 1;
 productController.createProduct = async (req, res) => {
     try {
         const { sku, name, image, price, description, stock, category, status } = req.body;
@@ -25,9 +25,21 @@ productController.createProduct = async (req, res) => {
 productController.getProducts = async (req, res) => {
     try {
         const { page, name } = req.query;
-        const cond = name ? { name: { $regex: name, $options: 'i' } } : {};
+
+        const cond = name ? { name: { $regex: name, $options: 'i' }, isDeleted: false } : { isDeleted: false };
         let query = Product.find(cond);
+        let res = { status: 'success' };
+        if (page) {
+            query = query.skip((page - 1) * PAGE_SIZE).limit(5); //最後の5データを見せる
+            //最終何ページがあるか
+            // 全データページデータが何データあるか
+            const totalItemNum = await Product.find(cond).count();
+            // 全てのデータ個数
+            const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+            res.totalPageNum = totalPageNum;
+        }
         const productList = await query.exec();
+        res.data = productList;
         // if (name){
         //     const products =await Product.find({name:{$regex:name,$option:"i"}})
         // }else {
@@ -41,7 +53,7 @@ productController.getProducts = async (req, res) => {
         //     }
         //}
         // const products = await Product.find({});
-        res.status(200).json({ status: 'succuss', data: productList });
+        return res.status(200).json(res);
     } catch (err) {
         return res.status(400).json({ status: 'fail', error: err.message });
     }

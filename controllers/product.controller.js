9a36c5312b6974dbf3c32a8b73fe9ae1160d691b
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-01-27 16:12:48 +0900

    pagenation

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Product = require('../models/Product');
 const productController = {};
-const PAGE_SIZE = 1;
+const PAGE_SIZE = 5;
 productController.createProduct = async (req, res) => {
     try {
         const { sku, name, image, price, description, stock, category, status } = req.body;
@@ -27,12 +27,12 @@ productController.getProducts = async (req, res) => {
         ///重複条件をまとめてやる
         let cond = name ? { name: { $regex: name, $options: 'i' } } : {};
         let query = Product.find(cond);
-        // if (page) {
-        //     query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
-        //     const totalItemNum = await Product.find(cond).count();
-        //     const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
-        //     response.data = productList;
-        // }
+        if (page) {
+            query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+            const totalItemNum = await Product.find(cond).count();
+            const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+            response.data = productList;
+        }
         // if (name) {
         //     const products = await Product.find({ name: { $regex: name, $options: 'i' } });
         // } else {

ba085863b3f5ecc12c4f6a1a3d360fc4f76d3d6f
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-02-03 17:41:49 +0900

    backend_detail

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -26,29 +26,23 @@ productController.createProduct = async (req, res) => {
 productController.getProducts = async (req, res) => {
     try {
         const { page, name } = req.query;
+        let response = { status: 'success' };
         ///重複条件をまとめてやる
         const PAGE_SIZE = 8;
-        let skipItems = page ? (page - 1) * PAGE_SIZE : 0;
-        let cond = name ? { name: { $regex: name, $options: 'i' } } : {};
-        let query = Product.find(cond);
-        let response = { status: 'success' };
-        if (page) {
-            //const totalItemNum = await Product.find(cond).count();
-            const totalItemNum = await Product.countDocuments(cond);
-            const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
-            if (skipItems >= totalItemNum) {
-                return res.status(200).json({ status: 'success', data: [], message: 'No more products to display' });
-            }
 
-            query.skip(skipItems).limit(PAGE_SIZE);
+        let cond = name ? { name: { $regex: name, $options: 'i' } } : { isDeleted: false };
+        let query = Product.find(cond);
+
+        if (page) {
+            query = query.skip(page - 1).limit(PAGE_SIZE);
+            const totalItemNum = await Product.find(cond).count();
+            //const totalItemNum = await Product.countDocuments(cond);
+            const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+            // if (skipItems >= totalItemNum) {
+            //     return res.status(200).json({ status: 'success', data: [], message: 'No more products to display' });
+            // }
             response.totalPageNum = totalPageNum;
         }
-        // if (name) {
-        //     const products = await Product.find({ name: { $regex: name, $options: 'i' } });
-        // } else {
-        //     const products = await Product.find({});
-        // }
-
         const productList = await query.exec();
         response.data = productList;
         return res.status(200).json(response);
@@ -60,21 +54,8 @@ productController.getProducts = async (req, res) => {
 productController.detailProduct = async (req, res) => {
     try {
         const productId = req.params.id;
-        const productDetail = await Product.findOne(
-            { _id: productId },
-            {
-                sku: 1,
-                name: 1,
-                image: 1,
-                price: 1,
-                description: 1,
-                stock: 1,
-                category: 1,
-            }
-        );
-        if (!productDetail) {
-            return res.status(404).json({ status: 'fail', message: 'Product not found' });
-        }
+        const productDetail = await Product.findOne(productId);
+        if (!productDetail) throw new Error('No item found');
         res.status(200).json({ status: 'success', data: productDetail });
     } catch (err) {
         return res.status(400).json({ status: 'fail', error: err.message });
@@ -101,7 +82,7 @@ productController.updateProduct = async (req, res) => {
 productController.deleteProduct = async (req, res) => {
     try {
         const productId = req.params.id;
-        const productDelete = await Product.findByIdAndDelete({ _id: productId, isDeleted: true });
+        const productDelete = await Product.findByIdAndUpdate({ _id: productId }, { isDeleted: true });
         if (!productDelete) {
             throw new Error('Product not found or already deleted');
         }

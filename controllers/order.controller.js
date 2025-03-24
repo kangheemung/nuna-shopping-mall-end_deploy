1700d7e76e1064661861f4bac230e3e2d9b6d87a
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-24 23:07:58 +0900

    order

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,7 +1,6 @@
-
 const express = require('express');
 const Order = require('../models/Order');
-const{randomStringGenerator} =require("../utils/randomStringGenerator");
+const { randomStringGenerator } = require('../utils/randomStringGenerator');
 const productController = require('./product.controller');
 const orderController = {};
 
@@ -11,16 +10,11 @@ orderController.createOrder = async (req, res) => {
         const { userId } = req; //user
         const { shipTo, contact, orderList, totalPrice } = req.body;
         //在庫確認＆アップデート
-        const insufficientStockItems = await productController.checkItemListStock(
-            orderList
-            );
+        const insufficientStockItems = await productController.checkItemListStock(orderList);
         // 재고가 충분하지 않음 =>에러
         if (insufficientStockItems.length > 0) {
             //errorメッセージ
-            const errorMessage = insufficientStockItems.reduce(
-                (total, item) => (total += item.message),
-              ""
-            );
+            const errorMessage = insufficientStockItems.reduce((total, item) => (total += item.message), '');
             throw new Error(errorMessage);
         }
         // 재고 충분함 =>오더
@@ -37,80 +31,75 @@ orderController.createOrder = async (req, res) => {
         await newOrder.save();
         //save후에 카트를 비우자
 
-
-        res.status(200).json({status:"success",orderNum: newOrder.orderNum})
+        res.status(200).json({ status: 'success', orderNum: newOrder.orderNum });
     } catch (e) {
         return res.status(400).json({ status: 'fail', message: e.message });
     }
-
 };
 const PAGE_SIZE = 10;
 orderController.getOrder = async (req, res) => {
-  try {
-    const { userId } = req;
-    const orderList = await Order.find({ userId: userId }).populate({
-      path: "items",
-      populate: {
-        path: "productId",
-        model: "Product",
-        select: "image name",
-      },
-    });
-    const totalItemNum =orderList.length;
-    const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
-    res.status(200).json({ status: "success", data: orderList, totalPageNum });
-  } catch (error) {
-    console.error(error);
-    return res.status(400).json({ status: "fail", error: error.message });
-  }
+    try {
+        const { userId } = req;
+        const orderList = await Order.find({ userId: userId }).populate({
+            path: 'items',
+            populate: {
+                path: 'productId',
+                model: 'Product',
+                select: 'image name',
+            },
+        });
+        const totalItemNum = orderList.length;
+        const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+        res.status(200).json({ status: 'success', data: orderList, totalPageNum });
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({ status: 'fail', error: error.message });
+    }
 };
 orderController.getOrderList = async (req, res) => {
-  try {
+    try {
+        const { page, ordernum } = req.query;
+        let cond = {};
+        if (ordernum) {
+            cond = {
+                orderNum: { $regex: ordernum, $options: 'i' },
+            };
+        }
+        const orderList = await Order.find(cond)
+            .populate('userId')
+            .populate({
+                path: 'items',
+                populate: {
+                    path: 'productId',
+                    model: 'Product',
+                    select: 'image name',
+                },
+            })
+            .skip((page - 1) * PAGE_SIZE)
+            .limit(PAGE_SIZE);
+        const totalItemNum = await Order.countDocuments(cond);
 
-    const { page, ordernum } = req.query;
-    let cond = {};
-    if (ordernum) {
-      cond = {
-        orderNum: { $regex: ordernum, $options: "i" },
-      };
+        const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+        res.status(200).json({ status: 'success', data: orderList, totalPageNum });
+    } catch (error) {
+        return res.status(400).json({ status: 'fail', error: error.message });
     }
-    const orderList = await Order.find(cond)
-      .populate("userId")
-      .populate({
-        path: "items",
-        populate: {
-          path: "productId",
-          model: "Product",
-          select: "image name",
-        },
-      })
-      .skip((page - 1) * PAGE_SIZE)
-      .limit(PAGE_SIZE);
-      const totalItemNum = await Order.countDocuments(cond);
-
-    const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
-    res.status(200).json({ status: "success", data: orderList, totalPageNum });
-  } catch (error) {
-    return res.status(400).json({ status: "fail", error: error.message });
-  }
 };
 
-orderController.updateOrder = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { status } = req.body;
-    const order = await Order.findByIdAndUpdate(
-      id,
-      { status: status },
-      { new: true }
-    );
-    if (!order) throw new Error("Can't find order");
+orderController.updateOrder = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { status } = req.body;
+        if (!status) {
+            return res.status(400).json({ status: 'fail', error: 'Status is required' });
+        }
 
-    res.status(200).json({ status: "success", data: order });
-  } catch (error) {
-    return res.status(400).json({ status: "fail", error: error.message });
-  }
+        const order = await Order.findByIdAndUpdate(id, { status: status }, { new: true });
+
+        res.status(200).json({ status: 'success', data: order });
+    } catch (error) {
+        return res.status(400).json({ status: 'fail', error: error.message });
+    }
 };
 
 module.exports = orderController;
-
--- routes/order.api.js
+++ routes/order.api.js
@@ -4,14 +4,10 @@ const authController = require('../controllers/auth.controller');
 const orderController = require('../controllers/order.controller');
 //token
 //api/order
-router.post('/', authController.authenticate,orderController.createOrder);
-router.get('/me', authController.authenticate,orderController.getOrder);
-router.get("/", authController.authenticate, orderController.getOrderList);
-router.put(
-  "/:id",
-  authController.authenticate,
-  authController.checkAdminPermission,
-  orderController.updateOrder
-);
+router.post('/', authController.authenticate, orderController.createOrder);
+router.get('/', authController.authenticate, orderController.getOrderList);
+router.get('/me', authController.authenticate, orderController.getOrder);
+
+router.put('/:id', authController.authenticate, authController.checkAdminPermission, orderController.updateOrder);
 
 module.exports = router;
